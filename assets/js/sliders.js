4971e6a0a48772c95f3ab95150b04105265f2f6a
zeaglailson <jaglailson1@gmail.com>
2022-01-16 14:06:55 -0300

    section recentes finalizada

---
+++ assets/js/sliders.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    console.log('aqui')
+    $(".owl-carousel-servicos-recentes").owlCarousel({
+        items:4,
+        loop:true,
+        dots:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items: 2
+            },
+            520:{
+                items:2
+            },
+            720:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+})
